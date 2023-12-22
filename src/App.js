9bc78754aa257bc8cd8993dd46301fd74af58c85
mahesh <maheshlj093@gmail.com>
2023-12-22 23:33:47 +0530

    color changer

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Change from './components/Change'
+function App() {
+  return (
+    <>
+      <Change/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Change.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+function Change() {
+ const [color,setColor]=useState("red");
+ const [val,setVal]=useState(10);
+  return (
+    <>
+    <h1 className='text-white text-center border-l py-5 font-bold' style={{backgroundColor:color}}>Value:{val}</h1>
+    <div className='mt-5 flex justify-center'>
+      
+      <button className='h-10 px-6 mx-3 font-semibold rounded-md bg-black text-white' onClick={()=>setColor("yellow")}>yellow</button>
+      <button className='h-10 px-6 mx-3 font-semibold rounded-md bg-black text-white' onClick={()=>setColor("red")}>red</button>
+      <button className='h-10 px-6 mx-3 font-semibold rounded-md bg-black text-white' onClick={()=>setColor("orange")}>orange</button>
+    <button className='h-10 px-6 mx-3 font-semibold rounded-md bg-black text-white' onClick={()=>{
+        setVal((prev)=>prev+1)
+    }}>increase</button>
+    </div>
+    </>
+
+  )
+}
+
+export default Change
